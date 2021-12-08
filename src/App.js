a4527ef8a24c3a96292a11421c7344c8f1caba23
qgatssdev <chivoomodu@gmail.com>
2021-12-08 09:06:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Movie from './Components/Movie';
+import { moviesList } from './Components/mockdata';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <input className="search" type="search" placeholder="search..." />
       </header>
-    </div>
+      <div className="movie-container">
+        {moviesList.map((moviesList) => (
+          <Movie key={moviesList.id} {...moviesList} />
+        ))}
+      </div>
+    </>
   );
 }
 
